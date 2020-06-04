04049aa7cc273bdab4e08a386e7323154405c199
Temarie <temarie@ya.ru>
2020-06-04 10:04:30 +0500

    auth interceptors

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import Vuelidate from 'vuelidate'
 import router from './router'
 import store from './store'
 import { sync } from 'vuex-router-sync'
+import axios from 'axios/index'
+import { getCookie, deleteCookie } from './utils'
 
 Vue.config.productionTip = false
 
@@ -14,6 +16,43 @@ Vue.use(Vuex)
 Vue.use(Vuelidate)
 
 sync(store, router)
+
+// request interceptor
+const createSetAuthInterceptor = () => config => {
+  if (getCookie('token') !== null) {
+    config.headers.Authorization = `Bearer ${getCookie('token')}`
+  }
+  return config
+}
+
+const setAuthCb = createSetAuthInterceptor(getCookie('token'))
+axios.interceptors.request.use(setAuthCb)
+
+// response interceptor
+let refreshTokenPromise
+
+const createUpdateAuthInterceptor = (store, http) => async error => {
+  if (error.response.status === 500 &&
+    (error.response.config.url.indexOf('refresh') > -1 || error.response.config.url.indexOf('logout'))) {
+    deleteCookie('token')
+    window.location.href = `${process.env.VUE_APP_DOMAIN}/login`
+  }
+  if (error.response.data.error.code !== 401) {
+    return Promise.reject(error)
+  }
+
+  if (!refreshTokenPromise) {
+    refreshTokenPromise = store.dispatch('User/refreshToken')
+  }
+
+  await refreshTokenPromise
+  refreshTokenPromise = null
+
+  return http(error.config)
+}
+
+const updateAuthCb = createUpdateAuthInterceptor(store, axios)
+axios.interceptors.response.use(null, updateAuthCb)
 
 new Vue({
   router: router,
