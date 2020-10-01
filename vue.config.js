9e930995e93deb86314386d70f4f63ddb6e3f414
Alexandr Grebnev <42351762+grebnev-av@users.noreply.github.com>
2020-10-01 11:22:38 +0300

    [PTH-14166] pre render site (#19)

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,33 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: require('./aliases.config').webpack
     },
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [
+          '/',
+          '/login',
+          '/signup',
+          '/restore',
+          '/after-sign-up',
+          '/account',
+          '/policy',
+          '/terms',
+          '/about',
+          '/features',
+          '/oauth2',
+          '/ui',
+          '/404',
+        ],
+      })
+    ]
   },
   pwa: {
     name: 'Ptah — landing page builder for games.',
